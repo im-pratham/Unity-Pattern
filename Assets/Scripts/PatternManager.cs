36447dcd21ebab76ec6976b9ba7900caeb2f62d7
im-pratham <prathameshmane1234@gmail.com>
2019-03-09 22:33:45 +0530

    Initial Commit

---
+++ Assets/Scripts/PatternManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatternManager : MonoBehaviour
+{
+    [Tooltip("Mesh on which pattern to be rendered")]
+    public MeshRenderer meshRenderer;
+
+    public Material gridMaterial;
+    public Material stripMaterial;
+    
+    // 2 is the possibe minimum value we defined in shader (Max value is 50)
+    private float currentDensity = 2;
+
+    public void SetPatternDensity (float density)
+    {
+        meshRenderer.material.SetFloat("_Density", density);
+        currentDensity = density;
+    }
+
+    public void DisplayGridPattern()
+    {
+        meshRenderer.material = gridMaterial;
+        SetPatternDensity(currentDensity);
+    }
+
+    public void DisplayStripPattern()
+    {
+        meshRenderer.material = stripMaterial;
+        SetPatternDensity(currentDensity);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initial setup
+        DisplayGridPattern();
+        SetPatternDensity(currentDensity);
+    }
+}
---
+++ Assets/Shaders/Demo/DemoColor.shader
@@ -0,0 +1,19 @@
+﻿Shader "Demo/Color"
+{
+    Properties
+    {
+        _Color("Main Color", Color) = (1, 0.5, 0.5, 1)
+    }
+    SubShader
+    {
+        Pass
+        {
+            Material 
+			{
+				Diffuse [_Color]
+			}
+
+			Lighting On
+        }
+    }
+}
